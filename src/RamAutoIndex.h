44645ddd7f3e46e394d90575537ae11d8d5e5548
Scyptnex <nic.h.misc@gmail.com>
2016-05-19 11:02:09 +1000

    License changes

--- src/RamAutoIndex.h
+++ src/RamAutoIndex.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved
  * Licensed under the Universal Permissive License v 1.0 as shown at:
  * - https://opensource.org/licenses/UPL
- * - souffle/LICENSE
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
  */
 
 /***************************************************************************
