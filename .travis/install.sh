8ec77d62753dab6151429ae1df52fd76984f7fff
Paul Subotic <paul@Pauls-MacBook-Air-2.local>
2016-09-26 17:59:57 -0700

    attemting fix of travis issue

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,7 +12,7 @@ set -x
 # Install requirements of Linux
 if [ $TRAVIS_OS_NAME == linux ]
 then
-   sudo apt-get -y install debhelper devscripts build-essential g++ automake autoconf bison flex openjdk-7-jdk libboost-all-dev lsb-release
+   sudo apt-get -y install debhelper devscripts build-essential g++ automake autoconf bison flex openjdk-7-jdk libboost-all-dev lsb-release libtool
 fi
 
 ############
@@ -23,6 +23,6 @@ fi
 if [ $TRAVIS_OS_NAME == osx ]
 then
    brew update
-   brew install md5sha1sum bison
+   brew install md5sha1sum bison libtool
    brew link bison --force
 fi
