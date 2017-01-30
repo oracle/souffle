2233be3fcc79bd724ce9aedb60e2359a218dc37b
lyndonhenry <lyndonhenrydotnet@gmail.com>
2017-01-30 13:00:39 +1100

    Monday 30 January  13:00:38 AEDT 2017

--- src/PrecedenceGraph.cpp
+++ src/PrecedenceGraph.cpp
@@ -270,6 +270,7 @@ void TopologicallySortedSCCGraph::reverseDFS(int sv, std::vector<int>& unordered
         }
         sccGraph->setColor(sv, BLACK);
         std::cerr << "----------------" << std::endl;
+        std::cerr << "Lookahead:      " << LOOKAHEAD << std::endl;
         std::cerr << "Unordered SCCs: " << unorderedSCCs << std::endl;
         std::cerr << "Ordered SCCs:   " << orderedSCCs << std::endl;
         std::cerr << "----------------" << std::endl;
