89bd9b70f10c39bbb83e7a7f19ede5c32a6ef869
Martin McGrane <mmcgrane@it.usyd.edu.au>
2017-02-01 16:15:52 +1100

    Mark const unescape function as const.

--- src/AstIODirective.h
+++ src/AstIODirective.h
@@ -118,7 +118,7 @@ protected:
         return other.name == name && other.input == input && other.kvps == kvps;
     }
 
-    std::string unescape(const std::string& inputString) {
+    std::string unescape(const std::string& inputString) const {
         std::string unescaped = unescape(inputString, "\\\"", "\"");
         unescaped = unescape(unescaped, "\\t", "\t");
         unescaped = unescape(unescaped, "\\r", "\r");
@@ -127,11 +127,12 @@ protected:
     }
 
     std::string unescape(
-            const std::string& inputString, const std::string& needle, const std::string replacement) {
+            const std::string& inputString, const std::string& needle, const std::string replacement) const {
         std::string result = inputString;
-        size_t pos;
-        while ((pos = result.find(needle)) != std::string::npos) {
+        size_t pos = 0;
+        while ((pos = result.find(needle, pos)) != std::string::npos) {
             result = result.replace(pos, needle.length(), replacement);
+            pos += replacement.length();
         }
         return result;
     }
