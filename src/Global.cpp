eecff88ae24505830b10824806bb3ef58537c097
lyndonhenry <lyndonhenrydotnet@gmail.com>
2017-02-08 17:16:53 +1100

    Wednesday 8 February  17:16:52 AEDT 2017

--- src/Global.cpp
+++ src/Global.cpp
@@ -6,7 +6,7 @@
 #define __ERROR_1__(text) { souffle::macro::call("Error", std::cerr, text); exit(1); }
 #endif
 #ifndef __ERROR_2__
-#define __ERROR_2__(text, callback) { std::cerr << "Error: " << message << std::endl; callback(); exit(1); }
+#define __ERROR_2__(text, callback) { std::cerr << "Error: " << text << std::endl; callback(); exit(1); }
 #endif
 #define GET_MACRO(_1, _2, NAME, ...) NAME
 #define ERROR(...) GET_MACRO(__VA_ARGS__, __ERROR_2__, __ERROR_1__)(__VA_ARGS__)
