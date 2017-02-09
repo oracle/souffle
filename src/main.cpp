92e9ddd86826e02be0708e72e8e7861e528ed399
lyndonhenry <lyndonhenrydotnet@gmail.com>
2017-02-09 15:32:56 +1100

    Thursday 9 February  15:32:56 AEDT 2017

--- src/main.cpp
+++ src/main.cpp
@@ -115,9 +115,9 @@ int main(int argc, char **argv)
                     {"verbose",       'v',     "",          "", false, "Verbose output."},
                     {"help",          'h',     "",          "", false, "Display this help message."},
 		    // TODO
-                    {"breadth-limit",   3,    "N",   "", false, "Specify the breadth limit used for the topological ordering of strongly connected components."},
-                    {"depth-limit",     4,    "N",   "", false, "Specify the depth limit used for the topological ordering of strongly connected components."},
-                    {"lookahead",       5,    "N",   "", false, "Specify the lookahead used for the topological ordering of strongly connected components."}
+                    {"breadth-limit",   3,    "N",          "", false, "Specify the breadth limit used for the topological ordering of strongly connected components."},
+                    {"depth-limit",     4,    "N",          "", false, "Specify the depth limit used for the topological ordering of strongly connected components."},
+                    {"lookahead",       5,    "N",          "", false, "Specify the lookahead used for the topological ordering of strongly connected components."}
                 };
                 return std::vector<MainOption>(std::begin(opts), std::end(opts));
             }()
@@ -136,22 +136,22 @@ int main(int argc, char **argv)
             ERROR("cannot open file " + std::string(argv[optind]));
 
 	// TODO
-	if (globalConfig.has("breadth-limit")) {
-	int limit = std::stoi(globalConfig.get("breadth-limit"));
+	if (Global::config().has("breadth-limit")) {
+	int limit = std::stoi(Global::config().get("breadth-limit"));
 	if (limit <= 0)
 	    fail("error: breadth limit must be 1 or more");
 	TopologicallySortedSCCGraph::BREADTH_LIMIT = limit;
 	}
-	if (globalConfig.has("depth-limit")) {
-	int limit = std::stoi(globalConfig.get("depth-limit"));
+	if (Global::config().has("depth-limit")) {
+	int limit = std::stoi(Global::config().get("depth-limit"));
 	if (limit <= 0)
 	    fail("error: depth limit must be 1 or more");
 	TopologicallySortedSCCGraph::DEPTH_LIMIT = limit;
 	}
-	if (globalConfig.has("lookahead")) {
-	if (globalConfig.has("breadth-limit") || globalConfig.has("depth-limit"))
+	if (Global::config().has("lookahead")) {
+	if (Global::config().has("breadth-limit") || Global::config().has("depth-limit"))
 	    fail("error: only one of either lookahead or depth-limit and breadth-limit may be specified");
-	int lookahead = std::stoi(globalConfig.get("lookahead"));
+	int lookahead = std::stoi(Global::config().get("lookahead"));
 	if (lookahead <= 0)
 	    fail("error: lookahead must be 1 or more");
 	TopologicallySortedSCCGraph::LOOKAHEAD = lookahead;
