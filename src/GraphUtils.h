215778f4ec65c49bd5333ad0f7f548b4ce42d04c
lyndonhenry <lyndonhenrydotnet@gmail.com>
2017-02-18 23:04:04 +1100

    TESTING

--- src/GraphUtils.h
+++ src/GraphUtils.h
@@ -186,6 +186,39 @@ class HyperGraph : public Graph<size_t> {
 
     public:
 
+//        template <typename Compare = std::less<Node>>
+//        static HyperGraph<Table, Node> toHyperGraph(Graph<Node, Compare> oldGraph) {
+//            HyperGraph<Table, Node> newGraph = HyperGraph<Table, Node>();
+//            size_t index = 0;
+//            for (const size_t vertex : oldGraph.allVertices()) {
+//                newGraph.insertVertex(index, vertex);
+//                index++;
+//            }
+//            for (const size_t vertex : oldGraph.allVertices()) {
+//                index = newGraph.vertexTable.getIndex(vertex);
+//                for (const size_t successor : oldGraph.getSuccessors(vertex)) {
+//                    newGraph.insertEdge(index, newGraph.vertexTable().getIndex(successor));
+//                }
+//            }
+//            return newGraph;
+//        }
+//
+//        template<template <typename> class OtherTable, typename OtherNode>
+//        static HyperGraph<Table, Node> toHyperGraph(HyperGraph<OtherTable, OtherNode> oldGraph) {
+//            HyperGraph<Table, Node> newGraph = HyperGraph<Table, Node>();
+//            size_t index = 0;
+//            for (const size_t vertex : oldGraph.allVertices()) {
+//                newGraph.insertVertex(vertex, vertex);
+//            }
+//            for (const size_t vertex : oldGraph.allVertices()) {
+//                index = newGraph.vertexTable.getIndex(vertex);
+//                for (const size_t successor : oldGraph.getSuccessors(vertex)) {
+//                    newGraph.insertEdge(index, newGraph.vertexTable().getIndex(successor));
+//                }
+//            }
+//            return newGraph;
+//        }
+
         const Table<Node>& vertexTable() {
             return this->table;
         }
@@ -262,69 +295,6 @@ class HyperGraph : public Graph<size_t> {
         }
 
 };
-
-class GraphTransform {
-
-private:
-
-   /*
-    * Compute strongly connected components using Gabow's algorithm (cf. Algorithms in
-    * Java by Robert Sedgewick / Part 5 / Graph *  algorithms). The algorithm has linear
-    * runtime.
-    */
-    template <template <typename> class Table, typename Node, typename Compare = std::less<Node>>
-    static void toSCCGraph(const Graph<Node, Compare>& graph, HyperGraph<Table, Node>& sccGraph, const Node& w, std::map<Node, int>& preOrder, size_t& counter, std::stack<Node>& S, std::stack<Node>& P) {
-
-        preOrder[w] = counter++;
-
-        S.push(w);
-        P.push(w);
-
-        for(const Node& t : graph.getPredecessors(w))
-            if (preOrder[t] == -1)
-                toSCCGraph(graph, sccGraph, t, preOrder, counter, S, P);
-            else if (!sccGraph.vertexTable().has(t))
-                while (preOrder[P.top()] > preOrder[t])
-                    P.pop();
-
-        if (P.top() == w)
-            P.pop();
-        else
-            return;
-
-        Node v;
-        size_t s = sccGraph.vertexCount();
-        sccGraph.insertVertex(s);
-        do {
-            v = S.top();
-            S.pop();
-            sccGraph.appendToVertex(s, v);
-        } while(v != w);
-    }
-
-
-public:
-
-    template <template <typename> class Table, typename Node, typename Compare = std::less<Node>>
-    static HyperGraph<Table, Node> toSCCGraph(const Graph<Node, Compare> graph) {
-            size_t counter = 0;
-            std::stack<Node> S, P;
-            std::map<Node, int> preOrder;
-            HyperGraph<Table, Node> sccGraph = HyperGraph<Table, Node>();
-            for (const Node& vertex : graph.allVertices())
-                   preOrder[vertex] = -1;
-            for (const Node& vertex : graph.allVertices())
-                if (preOrder[vertex]  == -1)
-                    toSCCGraph(graph, sccGraph, vertex, preOrder, counter, S, P);
-            for (const Node& vertex : graph.allVertices())
-                for (const Node& predecessor : graph.getPredecessors(vertex))
-                    if (vertex != predecessor && sccGraph.vertexTable().getIndex(vertex) != sccGraph.vertexTable().getIndex(predecessor))
-                        sccGraph.insertEdge(sccGraph.vertexTable().getIndex(vertex), sccGraph.vertexTable().getIndex(predecessor));
-            return sccGraph;
-    }
-
-};
-
 
 class GraphOrder {
 public:
