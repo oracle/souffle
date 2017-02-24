d6474c7c63ce913e7bd944ac2bad2e0d6d4b23f0
DominicRomanowski <romanowski.dominic@gmail.com>
2017-02-24 12:02:59 +1100

    working read of gui_src directly into cpp strings

--- src/profilerlib/html_string.hpp
+++ src/profilerlib/html_string.hpp
@@ -14,7 +14,6 @@
 
 #include "StringUtils.hpp"
 
-#define gui_directory = "gui_src"
 
 class html_string {
 private:
@@ -23,10 +22,80 @@ private:
 public:
     html_string() {
         std::string current_dir = __FILE__;
-        std::cout << "__FILE__ => " << current_dir << std::endl;
-        std::cout << "__BASE_FILE__ => " << __BASE_FILE__ << std::endl;
-        std::cout << "MAKEDIR => " << MAKEDIR << std::endl;
-        throw("memes");
+        std::string make_directory = MAKEDIR; // Variable set as flag in src/Makefile.am
+
+        std::vector<std::string> profiler_dir = Tools::split(current_dir, "/");
+
+        if (profiler_dir.size() == 0) {
+            std::cerr << "Error loading source profiler directory, ensure makefile contains the lines:\n"
+                         "DIR := ${CURDIR}"
+                         "souffle_profile_CXXFLAGS = $(souffle_CPPFLAGS) -DMAKEDIR='\"$(DIR)\"'";
+        }
+
+        std::string gui_directory = make_directory;
+
+        for (int i=0;i<profiler_dir.size()-1;++i) {
+            gui_directory += "/" + profiler_dir.at(i);
+        }
+
+        std::string GUI_DIR = "gui_src"; // TODO: change from hard coded string
+
+        gui_directory += "/" + GUI_DIR + "/";
+
+        std::cout << gui_directory << std::endl;
+
+        bool adding_to_first = true;
+
+        std::ifstream infile(gui_directory + "main.html");
+        std::string line;
+        std::string output;
+        while (std::getline(infile, line)) {
+            output = "";
+            if (line.find("<link") == 0) {
+                std::vector<std::string> src = Tools::split(line,"href=\"");
+                if (src.size()>1) {
+                    output = "<style>\n";
+                    std::ifstream infile2(gui_directory + Tools::split(src.at(1),"\"").at(0));
+                    while (std::getline(infile2, line)) {
+                        output += line + "\n";
+                    }
+                    output += "\n</style>\n";
+                    std::cout << output;
+                } else {
+                    output = line;
+                }
+            } else if (line.find("<script") == 0) {
+                std::vector<std::string> src = Tools::split(line,"src=\"");
+                if (src.size()>1) {
+                    output = "<script>\n";
+                    std::string filename = Tools::split(src.at(1),"\"").at(0);
+                    if (filename == "testtabledata.js") { // TODO: another hard coded string
+                        this->first_half += output;
+                        adding_to_first = false;
+                        output = "\n</script>\n";
+                    } else {
+                        std::ifstream infile2(gui_directory + filename);
+                        while (std::getline(infile2, line)) {
+                            output += line + "\n";
+                        }
+                        output += "\n</script>\n";
+                        std::cout << output;
+                    }
+                } else {
+                    output = line;
+                }
+            } else {
+                output = line;
+            }
+            if (output.empty())
+                continue;
+
+            if (adding_to_first)
+                this->first_half += output;
+            else
+                this->second_half += output;
+        }
+
     }
 
     inline std::string get_first_half() {
